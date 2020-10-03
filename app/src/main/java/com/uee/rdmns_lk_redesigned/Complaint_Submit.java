bc629850698b10b6bedf0531a1f1ba10a1996e84
lahiruferreira <lahriuferreira444@gmail.com>
2020-10-03 23:30:07 +0530

    Submit Complaints UI

---
+++ app/src/main/java/com/uee/rdmns_lk_redesigned/Complaint_Submit.java
@@ -0,0 +1,14 @@
+package com.uee.rdmns_lk_redesigned;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Complaint_Submit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint__submit);
+    }
+}
