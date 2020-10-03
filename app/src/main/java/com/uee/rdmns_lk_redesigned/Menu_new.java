1285bb68315872efccd31ef610e66b308bea1fb7
lahiruferreira <lahriuferreira444@gmail.com>
2020-10-03 22:49:15 +0530

    initial commit

---
+++ app/src/main/java/com/uee/rdmns_lk_redesigned/Menu_new.java
@@ -0,0 +1,14 @@
+package com.uee.rdmns_lk_redesigned;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_new extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_new);
+    }
+}
