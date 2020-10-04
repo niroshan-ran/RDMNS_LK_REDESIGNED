6131e8e3c464cb5488c8e93e561b922cd0ea642f
lahiruferreira <lahriuferreira444@gmail.com>
2020-10-04 22:30:32 +0530

    security no,Station details, complaints page navigation

---
+++ app/src/main/java/com/uee/rdmns_lk_redesigned/Contact_Stations.java
@@ -0,0 +1,14 @@
+package com.uee.rdmns_lk_redesigned;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contact_Stations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact__stations);
+    }
+}
--- app/src/main/java/com/uee/rdmns_lk_redesigned/Menu_new.java
+++ app/src/main/java/com/uee/rdmns_lk_redesigned/Menu_new.java
@@ -2,13 +2,50 @@ package com.uee.rdmns_lk_redesigned;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu_new extends AppCompatActivity {
+
+    private Button btnMenuComplaint,btnSecurity,btnStations;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_new);
+
+        btnMenuComplaint = (Button) findViewById(R.id.btnSubmitComplaints);
+        btnMenuComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_new.this,Complaint_Submit.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSecurity = (Button) findViewById(R.id.btnContactSecurity);
+        btnSecurity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_new.this,Contact_Security.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStations = (Button) findViewById(R.id.btnContactsOfStations);
+        btnStations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_new.this,Contact_Stations.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
