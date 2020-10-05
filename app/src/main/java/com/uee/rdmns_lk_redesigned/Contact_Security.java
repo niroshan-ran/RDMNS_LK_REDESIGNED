1075389d16ed97ace1ff439a5895e5e11dee7e0c
lahiruferreira <lahriuferreira444@gmail.com>
2020-10-05 21:06:12 +0530

    send email, phone call

--- app/src/main/java/com/uee/rdmns_lk_redesigned/Contact_Security.java
+++ app/src/main/java/com/uee/rdmns_lk_redesigned/Contact_Security.java
@@ -2,13 +2,35 @@ package com.uee.rdmns_lk_redesigned;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class Contact_Security extends AppCompatActivity {
 
+    EditText phoneNo;
+    ImageButton btnCall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact__security);
+
+        phoneNo = findViewById(R.id.phoneNumber);
+        btnCall = findViewById(R.id.phoneCall);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = phoneNo.getText().toString();
+                String s = "tel:"+ phone;
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(s));
+                startActivity(intent);
+
+            }
+        });
     }
 }
