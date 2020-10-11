7cb887291715cd85bb0e07f18b1937b43b4e7451
niroshan-ran <inranasinghe2208@gmail.com>
2020-10-11 19:17:30 +0530

    Submit News Updated...

--- app/src/main/java/com/uee/rdmns_lk_redesigned/SubmitNews.java
+++ app/src/main/java/com/uee/rdmns_lk_redesigned/SubmitNews.java
@@ -1,17 +1,17 @@
 package com.uee.rdmns_lk_redesigned;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SubmitNews extends AppCompatActivity {
 
     ImageButton btnSubmitNews, btnClearValues;
     EditText txtSubject, txtDescription;
-
 
 
     @Override
@@ -25,5 +25,31 @@ public class SubmitNews extends AppCompatActivity {
 
         btnSubmitNews = findViewById(R.id.btnSubmitNews);
         btnClearValues = findViewById(R.id.btnClearValues);
+
+        btnSubmitNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!txtSubject.getText().toString().trim().equals("") && !txtDescription.getText().toString().trim().equals("")) {
+                    txtSubject.setText("");
+                    txtDescription.setText("");
+                    Toast.makeText(SubmitNews.this, "News Successfully Submitted", Toast.LENGTH_SHORT).show();
+
+                } else
+                    Toast.makeText(SubmitNews.this, "Please Enter all the Required Fields", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        btnClearValues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtSubject.setText("");
+                txtDescription.setText("");
+            }
+        });
     }
+
+
 }
